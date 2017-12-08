1eff3c58cbbfb174d973f950ab656df3d71ba9c4
huoding <huo.ding@qq.com>
2017-12-08 17:26:18 +0800

    改测试时间

--- packages/ddn-node-sdk/lib/time/slots.js
+++ packages/ddn-node-sdk/lib/time/slots.js
@@ -8,8 +8,9 @@ function getEpochTime(time) {
 }
 
 function beginEpochTime() {
-	var d = new Date(Date.UTC(2017, 11, 20, 12, 20, 20, 20))
-
+	// var d = new Date(Date.UTC(2017, 11, 20, 12, 20, 20, 20))
+	var d = new Date(Date.UTC(2017, 10, 20, 12, 20, 20, 20));
+	
 	return d;
 }
 
