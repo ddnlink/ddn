7ef733c8cce932a21f129bf1c0960ac216ab4033
wangxm <softwaiter@126.com>
2018-10-26 17:42:00 +0800

    '增加verify逻辑，判断ipid唯一性'

--- packages/ddn-evidence/lib/asset-evidence.js
+++ packages/ddn-evidence/lib/asset-evidence.js
@@ -15,6 +15,35 @@ class AssetEvidence extends AssetBase
             {field: "str2", prop: "size"}
         ];
     }
+
+    verify(trs, sender, cb)
+    {
+        super.verify(trs, sender, (err, trans) => {
+            if (!err)
+            {
+                var condition = {
+                    filter: {
+                        ipid: trans.asset.assetEvidence.ipid
+                    }
+                }
+                super.queryAsset(condition, (err2, rows) => {
+                    if (err) {
+                        return cb(err2);
+                    }
+
+                    if (rows && rows.length > 0) {
+                        return cb('Evidence IPID already exists');
+                    } else {
+                        return cb(null, trans);
+                    }
+                })
+            }
+            else
+            {
+                return cb(err);
+            }
+        })
+    }
 }
 
 module.exports = AssetEvidence;
