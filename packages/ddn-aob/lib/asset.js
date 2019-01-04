9cfc850ecc2dc2855873673297a1c5c0cbfc6dd0
王力宇 <915153147@qq.com>
2019-01-04 15:22:50 +0800

    完善asset，缺少查询判断

--- packages/ddn-aob/lib/asset.js
+++ packages/ddn-aob/lib/asset.js
@@ -1,4 +1,5 @@
 const { AssetBase } = require('ddn-asset-base');
+const amountHelper = require('ddn-tools')
 
 class Asset extends AssetBase {
   propsMapping() {
@@ -53,5 +54,71 @@ class Asset extends AssetBase {
       },
     ];
   }
+
+  create(data, trs) {
+    trs.recipient_id = null
+    trs.amount = "0";
+    trs.asset.aobAsset = {
+      name: data.name,
+      desc: data.desc, 
+      maximum: data.maximum,
+      precision: data.precision,
+      strategy: data.strategy,
+      allow_writeoff: data.allow_writeoff,
+      allow_whitelist: data.allow_whitelist,
+      allow_blacklist: data.allow_blacklist
+    }
+    return trs;
+  }
+
+  calculateFee(trs, sender) {
+    return bignum.multiply(500, this.library.tokenSetting.fixedPoint);
+  }
+
+  verify(trs, sender, cb) {
+    super.verify(trs, senderm, async(err, trans) => {
+      if (err) {
+        return cb(err);
+      }
+
+      const asset = trs.asset.aobAsset;
+      const nameParts = (asset.name || '').split('.');
+      if (nameParts.length != 2) return setImmediate(cb, 'Invalid asset full name')
+
+      const fullName = asset.name;
+      const issuerName = nameParts[0];
+      const tokenName = nameParts[1];
+      if (!tokenName || !/^[A-Z]{3,6}$/.test(tokenName)) return setImmediate(cb, 'Invalid asset currency name')
+      if (!asset.desc) return setImmediate(cb, 'Invalid asset desc')
+      if (asset.desc.length > 4096) return setImmediate(cb, 'Invalid asset desc size')
+
+      if (asset.precision > 16 || asset.precision < 0) return setImmediate(cb, 'Invalid asset precision')
+
+      const error = amountHelper.validate(asset.maximum);
+      if (error) return setImmediate(cb, error)
+      if (asset.strategy && asset.strategy.length > 256) return setImmediate(cb, 'Invalid asset strategy size')
+      if (asset.allow_writeoff !== 0 && asset.allow_writeoff !== 1) return setImmediate(cb, 'Asset allowWriteoff is not valid')
+      if (asset.allow_whitelist !== 0 && asset.allow_whitelist !== 1) return setImmediate(cb, 'Asset allowWhitelist is not valid')
+      if (asset.allow_blacklist !== 0 && asset.allow_blacklist !== 1) return setImmediate(cb, 'Asset allowBlacklist is not valid')
+
+      try {
+        const where = { name: asset.name }
+        const orders = null;
+        const returnTotal = null;
+        const pageIndex = 1;
+        const pageSize = 1;
+        const assetData = await super.queryAsset(where, orders, returnTotal, pageIndex, pageSize);
+        if (assetData && assetData.length > 0) {
+          cb('asset->name Double register');
+        }
+
+        // 缺少更多判断
+
+      } catch (err2) {
+        cb(err2)
+      }
+    })
+  }
+
 }
 module.exports = Asset;
