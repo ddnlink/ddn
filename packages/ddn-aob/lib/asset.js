560f996319b08ddfced09d8f04692694624915dd
王力宇 <915153147@qq.com>
2019-01-04 17:23:21 +0800

    asset 100%

--- packages/ddn-aob/lib/asset.js
+++ packages/ddn-aob/lib/asset.js
@@ -82,66 +82,43 @@ class Asset extends AssetBase {
       if (err) {
         return cb(err);
       }
-
       const asset = trs.asset.aobAsset;
       const nameParts = (asset.name || '').split('.');
       if (nameParts.length != 2) return setImmediate(cb, 'Invalid asset full name form ddn-aob')
-
       const fullName = asset.name;
       const issuerName = nameParts[0];
       const tokenName = nameParts[1];
       if (!tokenName || !/^[A-Z]{3,6}$/.test(tokenName)) return setImmediate(cb, 'Invalid asset currency name form ddn-aob')
       if (!asset.desc) return setImmediate(cb, 'Invalid asset desc form ddn-aob')
       if (asset.desc.length > 4096) return setImmediate(cb, 'Invalid asset desc size form ddn-aob')
-
       if (asset.precision > 16 || asset.precision < 0) return setImmediate(cb, 'Invalid asset precision form ddn-aob')
-
-
-      console.log('ddnUtils.Amount.validate', ddnUtils.Amount.validate)
-
       const error = ddnUtils.Amount.validate(asset.maximum);
       if (error) return setImmediate(cb, error)
-
       if (asset.strategy && asset.strategy.length > 256) return setImmediate(cb, 'Invalid asset strategy size form ddn-aob')
       if (asset.allow_writeoff !== '0' && asset.allow_writeoff !== '1') return setImmediate(cb, 'Asset allowWriteoff is not valid form ddn-aob')
       if (asset.allow_whitelist !== '0' && asset.allow_whitelist !== '1') return setImmediate(cb, 'Asset allowWhitelist is not valid form ddn-aob')
       if (asset.allow_blacklist !== '0' && asset.allow_blacklist !== '1') return setImmediate(cb, 'Asset allowBlacklist is not valid form ddn-aob')
-
       try {
         const where = { name: asset.name }
         const orders = null;
         const returnTotal = null;
         const pageIndex = 1;
         const pageSize = 1;
-
-        console.log('第一次去查询')
-
         let assetData = await super.queryAsset(where, orders, returnTotal, pageIndex, pageSize);
         if (assetData && assetData.length > 0) {
           cb('asset->name Double register form ddn-aob');
         }
         assetData = assetData[0];
-        console.log('第二次去查询')
-
-        // 缺少更多判断
         const issuerWhere = { name: issuerName };
         let issuerData = await super.queryAsset(issuerWhere, orders, returnTotal, pageIndex, pageSize, 75);
-
         if (issuerData && issuerData.length > 0) {
           issuerData = issuerData[0]
         } else {
           return cb('Issuer not exists form ddn-aob')
         }
-        console.log('issuerData:', issuerData)
-        console.log('sender:', sender)
         if (issuerData.issuer_id != sender.address) return cb('Permission not allowed form ddn-aob');
-
         return cb(null);
-
       } catch (err2) {
-
-        console.log('err2', err2)
-
         cb(err2);
       }
     })
