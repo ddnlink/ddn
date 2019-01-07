fcb673156aa49a45e28422f721080c3023590fc0
王力宇 <915153147@qq.com>
2019-01-07 16:50:51 +0800

    新增calcRealAmount方法

--- packages/ddn-utils/lib/amount.js
+++ packages/ddn-utils/lib/amount.js
@@ -3,20 +3,29 @@ const amount = {
   validate(amount) {
     if (typeof amount != 'string') return 'Invalid amount type'
     if (!/^[1-9][0-9]*$/.test(amount)) return 'Amount should be integer'
-
     let bnAmount;
     try {
       bnAmount = bignum.new(amount);
     } catch (e) {
       return 'Failed to convert'
     }
-    
     if (bignum.isLessThan(bnAmount, 1) || 
         bignum.isGreaterThan(bnAmount, '1e48')) {
         return 'Invalid amount range'
     }
-
     return null
+  },
+  calcRealAmount(amount, precision) {
+    let ba = bignum.new(amount);
+    while (precision > 0) {
+      if (precision > 8) {
+        ba = bignum.divide(ba, 10 ** 8);
+      } else {
+        ba = bignum.divide(ba, 10 ** precision);
+      }
+      precision -= 8;
+    }
+    return ba.toString();
   }
 }
 
