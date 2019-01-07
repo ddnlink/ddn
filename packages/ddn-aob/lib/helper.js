b37f8d8c49193acd9942f4e21293d58c05a418b8
王力宇 <915153147@qq.com>
2019-01-07 17:55:27 +0800

    完善issue,新增helper

---
+++ packages/ddn-aob/lib/helper.js
@@ -0,0 +1,77 @@
+const { AssetBase } = require('ddn-asset-base');
+const _ = require('underscore');
+const ddnUtils = require('ddn-utils');
+
+class helper extends AssetBase {
+  async getAssets(where, pageIndex, pageSize, cb) {
+    new Promise(async () => {
+      try {
+        let result; // 最后返回的值
+        // (1)查询到asset的数据列表
+        result = await super.queryAsset(where, null, null, pageIndex, pageSize, 76);
+        // (2)查询到issuer的数据列表
+        let issuerData = await super.queryAsset({ '$in': _.pluck(result, 'issuer_name') }, null, null, 1, 1000, 75);
+        issuerData = _.indexBy(issuerData, 'name');
+        result = _.map(result, num => {
+          num.issuer_id = issuerData[num.issuer_name].issuer_id;
+          return num;
+        });
+        // (3)查询到交易的相关数据
+        let trData = await new Promise((reslove, reject) => {
+          this.library.dao.findList('tr', { id: { '$in': _.pluck(result, 'transaction_id')}}, 1000, null, null, ['id', 'block_id'], null, (err, data) => {
+            if (err) {
+              return reject(err);
+            }
+            reslove(data)
+          });
+        });
+        trData = _.indexBy(trData, 'id');
+        result = _.map(result, num => {
+          num.block_id = trData[num.transaction_id].block_id;
+          return num;
+        });
+        // (4)查询到块的相关数据
+        let blockData = await new Promise((reslove, reject) => {
+          this.library.dao.findList('block', { id: { '$in': _.pluck(result, 'block_id')}}, 1000, null, null, ['height', 'id'], null, (err, data) => {
+            if (err) {
+              return reject(err);
+            }
+            reslove(data)
+          });
+        })
+        const blockData = _.indexBy(blockData, 'id');
+        result = _.map(result, num => {
+          num.height = blockData[num.block_id].height;
+          return num;
+        });
+        // 循环整合验证数据
+        for (let i = 0; i < result.length; ++i) {
+          let precision = result[i].precision
+          result[i].maximum = bignum.new(result[i].maximum).toString(10);
+          result[i].maximumShow = ddnUtils.Amount.calcRealAmount(result[i].maximum, precision);
+          result[i].quantity = bignum.new(result[i].quantity).toString(10);
+          result[i].quantityShow = ddnUtils.Amount.calcRealAmount(result[i].quantity, precision);
+        }
+        // 返回最终值
+        cb(null, result)
+      } catch(e){
+        cb(e)
+      }
+    });
+  }
+
+  async addAssetQuantity(currency, amount, dbTrans, cb) {
+    if (typeof(cb) == "undefined" && typeof(dbTrans) == "function") {
+			cb = dbTrans;
+			dbTrans = null;
+    };
+
+    const data = await super.queryAsset({ name: currency }, null, null, pageIndex, pageSize, 76);
+
+    const quantity = data.quantity;
+
+
+  }
+
+}
+module.exports = helper;
--- packages/ddn-aob/lib/issue.js
+++ packages/ddn-aob/lib/issue.js
@@ -1,4 +1,8 @@
 const { AssetBase } = require('ddn-asset-base');
+const bignum = require('bignum-utils');
+const ddnUtils = require('ddn-utils');
+const mathjs = require('mathjs');
+const helper = require('./helper');
 
 class Issue extends AssetBase {
   propsMapping() {
@@ -13,5 +17,69 @@ class Issue extends AssetBase {
     }
     ];
   }
+
+  create(data, trs) {
+    trs.recipient_id = null;
+    trs.amount = "0";
+    trs.asset.aobIssue = {
+      currency: data.currency,
+      amount: data.amount + ""
+    }
+    return trs;
+  }
+
+  verify(trs, sender, cb) {
+    super.verify(trs, sender, async (err, trans) => {
+      if (err) {
+        return cb(err);
+      }
+      // 检查参数
+      if (trs.recipient_id) return setImmediate(cb, 'Invalid recipient');
+      if (!bignum.isZero(trs.amount)) return setImmediate(cb, 'Invalid transaction amount');
+
+      const amount = trs.asset.aobIssue.amount;
+      const error = ddnUtils.Amount.validate(amount);
+      if (error) return cb(error);
+
+      try {
+        // (1)得到资产数据
+        const where = { name: trs.asset.aobIssue.currency }
+        const pageIndex = 1;
+        const pageSize = 1;
+        helper.getAssets(where, pageIndex, pageSize, (err, result) => {
+          if (err) return cb(`Database error: ${err} --- from ddn-aob -> issue.verify`);
+          if (!result) return cb('Asset not exists --- from ddn-aob -> issue.verify')
+          if (result.issuer_id !== sender.address) return cb('Permission not allowed --- from ddn-aob -> issue.verify')
+          if (result.writeoff) return cb('Asset already writeoff --- from ddn-aob -> issue.verify')
+          const maximum = result.maximum;
+          const quantity = result.quantity;
+          const precision = result.precision;
+          if (bignum.isGreaterThan(bignum.plus(quantity, amount), maximum)) return cb('Exceed issue limit --- from ddn-aob -> issue.verify');
+          const strategy = result.strategy;
+          const genesisHeight = result.height;
+          const height = bignum.plus(modules.blocks.getLastBlock().height, 1);
+          if (strategy) {
+            try {
+              const context = {
+                maximum: mathjs.bignumber(maximum),
+                precision,
+                quantity: mathjs.bignumber(quantity).plus(amount),
+                genesisHeight: mathjs.bignumber(genesisHeight), //bignum update
+                height: mathjs.bignumber(height.toString()) //bignum update
+              };
+              var evalRet = mathjs.eval(strategy, context);
+              if (!evalRet) return cb('Strategy not allowed --- from ddn-aob -> issue.verify');
+            } catch (e) {
+              return cb('Failed to execute strategy --- from ddn-aob -> issue.verify')
+            }
+          }
+          return cb()
+        })
+      } catch (err2) {
+        cb(err2, ' --- from ddn-aob -> issue.verify');
+      }
+    })
+  }
+  
 }
 module.exports = Issue;
