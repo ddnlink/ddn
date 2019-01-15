dc471d91257757433a9f9c5164b2656499baa973
王力宇 <915153147@qq.com>
2019-01-15 17:16:47 +0800

    修改bug

--- packages/ddn-aob/getModels.js
+++ packages/ddn-aob/getModels.js
@@ -1,14 +1,22 @@
 var fs = require("fs");
+const path = require('path')
 const arr = [];
-
-const files = fs.readdirSync("./models")
-
+const files = fs.readdirSync(path.resolve(__dirname, './models/'));
 files.forEach((file) => {
   const item = require(path.resolve(__dirname, './models/', file))
   arr.push({
-    name:file,
+    name:splitFileName(file),
     data:item
   });
 });
+// 如果文件带有后缀名则去掉
+function splitFileName(text) {
+  var pattern = /\.{1}[a-z]{1,}$/;
+  if (pattern.exec(text) !== null) {
+    return (text.slice(0, pattern.exec(text).index));
+  } else {
+    return text;
+  }
+}
 
 module.exports = arr; 
