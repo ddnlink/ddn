4f84d7943982d854f687d8c9a927d43c75dc2c95
王力宇 <915153147@qq.com>
2019-01-15 16:49:24 +0800

    修改入口文件

--- packages/ddn-aob/index.js
+++ packages/ddn-aob/index.js
@@ -3,5 +3,7 @@ module.exports = {
   AobAsset: require('./lib/Asset'),       // type: 76
   AobIssue: require('./lib/Issue'),       // type: 77
   AobTransfer: require('./lib/Transfer'), // type: 78
-  MemAssetBalance: require('./lib/memAssetBalance'), // type: 79
+  AobAcl: require('./lib/acl'), // type: 79
+  AobFlag: require('./lib/flags'), // type: 80
+  AobFlagHelper: require('./lib/flagsHelper'), // type: 81
 }
