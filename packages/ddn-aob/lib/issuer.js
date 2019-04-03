2bc4f4dc5319b2b7045017752bd7b470eed16371
王力宇 <915153147@qq.com>
2019-04-03 16:58:17 +0800

    修改issuer

--- packages/ddn-aob/lib/issuer.js
+++ packages/ddn-aob/lib/issuer.js
@@ -1,107 +1,46 @@
-const { AssetBase } = require('ddn-asset-base');
+const {
+  AssetBase
+} = require('ddn-asset-base');
 const bignum = require('bignum-utils');
 
 class Issuer extends AssetBase {
-  propsMapping() {
+  async propsMapping() {
     return [{
-      field: "str1",
-      prop: "name",
-      required: true
-    },
-    {
-      field: "str2",
-      prop: "issuer_id"
-    },
-    {
-      field: "str10",
-      prop: "desc"
-    },
+        field: "str1",
+        prop: "name",
+        required: true
+      },
+      {
+        field: "str2",
+        prop: "issuer_id"
+      },
+      {
+        field: "str10",
+        prop: "desc"
+      },
     ];
   }
 
-  create(data, trs) {
-    trs.recipient_id = null;
-    trs.amount = "0";
-    trs.asset.aobIssuer = {
-      name: data.name,
-      desc: data.desc,
-      issuer_id: trs.sender_id,
-    }
-    return trs;
-  }
-
-  calculateFee() {
+  async calculateFee() {
     return bignum.multiply(100, this.library.tokenSetting.fixedPoint);
   }
 
-  verify(trs, sender, cb) {
+  async verify(trs, sender, cb) {
     // 先调用基类的验证
-    super.verify(trs, sender, async (err, trans) => {
-      if (err) {
-        return cb(err);
-      }
-      // 验证是否存在重复数据
-      try {
-        const issuer = trans.asset.aobIssuer;
-        const orders = null;
-        const returnTotal = null;
-        const pageIndex = 1;
-        const pageSize = 1;
-        const data1 = await super.queryAsset({ name: issuer.name }, orders, returnTotal, pageIndex, pageSize);
-        const data2 = await super.queryAsset({ issuer_id: trs.sender_id }, orders, returnTotal, pageIndex, pageSize);
-        const results = data1.concat(data2);
-        if (results && results.length > 0) {
-          cb('Evidence name/issuer_id already exists');
-        } else {
-          cb(null, trans);
-        }
-      } catch (err2) {
-        cb(err2);
-      }
-    })
-  }
-
-  apply(trs, block, sender, dbTrans, cb) {
-    if (typeof (cb) == "undefined" && typeof (dbTrans) == "function") {
-      cb = dbTrans;
-      dbTrans = null;
-    };
-    setImmediate(cb)
-  }
-
-  undo(trs, block, sender, dbTrans, cb) {
-    if (typeof (cb) == "undefined" && typeof (dbTrans) == "function") {
-      cb = dbTrans;
-      dbTrans = null;
-    };
-    setImmediate(cb)
-  }
-
-  applyUnconfirmed(trs, sender, dbTrans, cb) {
-    if (typeof (cb) == "undefined" && typeof (dbTrans) == "function") {
-      cb = dbTrans;
-      dbTrans = null;
-    };
-    const nameKey = `${trs.asset.aobIssuer.name}:${trs.type}`;
-    const idKey = `${sender.address}:${trs.type}`;
-    if (library.oneoff.has(nameKey) || library.oneoff.has(idKey)) {
-      return setImmediate(cb, 'Double submit')
+    var trans = await super.verify(trs, sender);
+    // 验证是否存在重复数据
+    const data1 = await super.queryAsset({
+      name: trans.asset.aobIssuer.name
+    }, null, null, 1, 1);
+    const data2 = await super.queryAsset({
+      issuer_id: trs.sender_id
+    }, null, null, 1, 1);
+    const results = data1.concat(data2);
+    if (results && results.length > 0) {
+      throw new Error('Evidence name/issuer_id already exists');
+    } else {
+      return trans;
     }
-    this.library.oneoff.set(nameKey, true)
-    this.library.oneoff.set(idKey, true)
-    setImmediate(cb)
-  }
-
-  undoUnconfirmed(trs, sender, dbTrans, cb) {
-    if (typeof (cb) == "undefined" && typeof (dbTrans) == "function") {
-      cb = dbTrans;
-      dbTrans = null;
-    };
-    const nameKey = `${trs.asset.aobIssuer.name}:${trs.type}`;
-    const idKey = `${sender.address}:${trs.type}`;
-    this.library.oneoff.delete(nameKey)
-    this.library.oneoff.delete(idKey)
-    setImmediate(cb)
   }
 
 }
