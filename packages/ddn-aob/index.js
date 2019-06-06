5c96ac6e9cc7252b489a78b5c428a07c564c2488
王力宇 <915153147@qq.com>
2019-06-06 14:58:34 +0800

    修改bug

--- packages/ddn-aob/index.js
+++ packages/ddn-aob/index.js
@@ -4,8 +4,8 @@ const Issue = require('./lib/issue');
 const Transfer = require('./lib/transfer');
 
 module.exports = {
-  Issuer, // type: 60
-  Asset, // type: 61
-  Issue, // type: 64
-  Transfer, // type: 65
+  AobIssuer: Issuer, // type: 60
+  AobAsset: Asset, // type: 61
+  AobIssue: Issue, // type: 64
+  AobTransfer: Transfer, // type: 65
 };
