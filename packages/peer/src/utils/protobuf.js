40984d6240edcd90c7e604d115bd091b3dbf921e
imfly <kubying@qq.com>
2020-08-14 17:27:14 +0800

    fix: 🐛 Fix publish error about @ddn/asset-dao v0.3.3

--- packages/peer/src/utils/protobuf.js
+++ packages/peer/src/utils/protobuf.js
@@ -4,7 +4,6 @@
  *  Copyright (c) 2019 DDN Foundation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *-------------------------------------------------------------------------------------------- */
-import fs from 'fs'
 import protobuf from 'protobufjs'
 
 import extend from 'extend'
