adeaf983a5a924f9e9941bedee14b6f9e473d5a7
tailor <zhuzhuowu@126.com>
2021-09-21 12:45:10 +0000

    refactor: 💡 remove export default of package core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -4,9 +4,9 @@ import { getUserConfig, requireFile, mergeConfigs } from './getUserConfig'
 export { getPaths, getUserConfig, requireFile, mergeConfigs }
 
 // todo: delete it
-export default {
-  getPaths,
-  getUserConfig,
-  requireFile,
-  mergeConfigs
-}
+// export default {
+//   getPaths,
+//   getUserConfig,
+//   requireFile,
+//   mergeConfigs
+// }
