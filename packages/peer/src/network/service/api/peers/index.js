29da2a36dd6dc603674f245331bd42a8549565fb
tailor <zhuzhuowu@126.com>
2022-08-02 02:18:01 +0000

    change the api of query peers

--- packages/peer/src/network/service/api/peers/index.js
+++ packages/peer/src/network/service/api/peers/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-prototype-builtins */
-import ip from 'ip'
 
 /**
  * RootRouter接口
@@ -12,87 +11,17 @@ class RootRouter {
   }
 
   async queryPeers (filter) {
-    const sortFields = ['ip', 'port', 'state', 'os', 'version']
-    let sortMethod = ''
-    let sortBy = ''
-    const limit = filter.limit || null
-    const offset = filter.offset || null
-
-    const where = {}
-    if (filter.hasOwnProperty('state') && filter.state !== null) {
-      where.state = filter.state
-    }
-    if (filter.hasOwnProperty('os') && filter.os !== null) {
-      where.os = filter.os
-    }
-    if (filter.hasOwnProperty('version') && filter.version !== null) {
-      where.version = filter.version
-    }
-    if (filter.hasOwnProperty('ip') && filter.ip !== null) {
-      where.ip = filter.ip
-    }
-    if (filter.hasOwnProperty('port') && filter.port !== null) {
-      where.port = filter.port
-    }
-    if (filter.hasOwnProperty('orderBy')) {
-      const sort = filter.orderBy.split(':')
-      sortBy = sort[0].replace(/[^\w\s]/gi, '')
-      if (sort.length === 2) {
-        sortMethod = sort[1] === 'desc' ? 'desc' : 'asc'
-      } else {
-        sortMethod = 'desc'
-      }
-    }
-
-    if (sortBy) {
-      if (!sortFields.includes(sortBy)) {
-        throw new Error('Invalid sort field')
-      }
-    }
-
-    if (limit !== null) {
-      if (limit > 100) {
-        throw new Error('Invalid limit. Maximum is 100')
-      }
-    }
-
-    return await this.dao.findPage('peer', {
-      where,
-      limit,
-      offset,
-      attributes: ['ip', 'port', 'state', 'os', 'version'],
-      order: sortBy ? [[sortBy, sortMethod]] : null
-    })
+    return this._context.runtime.peer.p2p.inventory.getAllPeers()
   }
 
   async get (req) {
-    const query = Object.assign({}, req.body, req.query)
-    // query.limit = Number(query.limit || 10)
-    // query.offset = Number(query.offset || 0)
-
-    // if (typeof query.state !== 'undefined') {
-    //   query.state = Number(query.state)
-    // }
-
-    // if (typeof query.port !== 'undefined') {
-    //   query.port = Number(query.port)
-    // }
-
-    const validateErrors = await this.ddnSchema.validatePeers(query)
-    if (validateErrors) {
-      throw new Error(`Invalid parameters 1: ${validateErrors[0].schemaPath} ${validateErrors[0].message}`)
-    }
-
     try {
-      const peers = await this.queryPeers(query)
-      for (let i = 0; i < peers.rows.length; i++) {
-        peers.rows[i].ip = ip.fromLong(peers.rows[i].ip)
-      }
+      const peers = this._context.runtime.peer.p2p.inventory.getAllPeers()
 
       return {
         success: true,
-        peers: peers.rows,
-        totalCount: peers.total
+        peers: peers,
+        totalCount: peers.length
       }
     } catch (err) {
       return {
@@ -129,15 +58,10 @@ class RootRouter {
     }
 
     try {
-      const peers = await this.queryPeers({
-        ip: query.ip,
-        port: query.port
-      })
+      const peers = this._context.runtime.peer.p2p.inventory.getAllPeers()
 
-      const peer = peers.rows && peers.rows.length ? peers.rows[0] : null
-      if (peer) {
-        peer.ip = ip.fromLong(peer.ip)
-      }
+      const peer = peers.find(item => item.ip === query.ip && item.port === query.port)
+
       return {
         success: true,
         peer: peer || {}
